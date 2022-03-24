cf9977e8e9fd22920682887e468bdf3cb5f42cfe
Ben <55573017+muranatsuradio@users.noreply.github.com>
2022-03-24 21:44:46 +0800

    Add

--- Assets/SHVFS_P103/Scripts/Enemy/EnemySimpleAI.cs
+++ Assets/SHVFS_P103/Scripts/Enemy/EnemySimpleAI.cs
@@ -20,10 +20,32 @@ public class EnemySimpleAI : MonoBehaviour
 {
     public EnemyState EnemyState;
 
+    public Transform StartPatrolPosition;
+    public Transform EndPatrolPosition;
+    public float MinDistance = 1.5f;
+    
+    private Vector3 _currentPatrolDestination;
     private NavMeshAgent _navMeshAgent;
 
     private void Start()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Patrol()
+    {
+        if (_navMeshAgent && _currentPatrolDestination != Vector3.zero)
+        {
+            _navMeshAgent.SetDestination(_currentPatrolDestination);
+        }
+
+        var distance = Vector3.Distance(_currentPatrolDestination, transform.position);
+        
+        if (distance < MinDistance)
+        {
+            _currentPatrolDestination = _currentPatrolDestination == EndPatrolPosition.position
+                ? StartPatrolPosition.position
+                : EndPatrolPosition.position;
+        }
     }
 }
